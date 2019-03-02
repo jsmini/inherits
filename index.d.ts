6af1737e05616e3526a586f27960eb7f530f44c4
颜海镜 <yanhaijing@yeah.net>
2019-03-02 23:10:12 +0800

    0.6.0

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export as namespace jsminiInherits;
+
+export function inherits(C: Function, P: Function)
+export function inheritsByES3(C: Function, P: Function)
+export function inheritsByForce(C: Function, P: Function)
